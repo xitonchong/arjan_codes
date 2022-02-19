4dea2c4da4edb2cd49901cf626b101d1700ba8ea
hentai_usagi <xitonchong@gmail.com>
2022-02-19 12:52:54 +0800

    protocol way of bridge pattern

---
+++ 2022-bridge/with_protocol/stream/device.py
@@ -0,0 +1,9 @@
+from typing import Protocol 
+
+from stream.data import BufferData 
+
+
+
+class StreamingDevice(Protocol):
+    def get_buffer_data(self) -> BufferData:
+        ...
---
+++ 2022-bridge/with_protocol/stream/dslr_camera.py
@@ -0,0 +1,5 @@
+from stream.data import BufferData 
+
+class DSLRCamera:
+    def get_buffer_data(self) -> BufferData:
+        return "####DSLR###"
